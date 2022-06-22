236aa2f106a53da2e0e3f2f93bb8c19c398b03d5
Keshav371 <kssharmaaz@gmail.com>
2022-06-22 11:30:28 -0700

    Third Commit

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,28 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let name = 'Flashy Fighter';
+let country = 'Ebanland';
+let villian = 'Demon King';
+let rank = 'Knight';
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let time = '1200';
+let location = 'Country';
+let companion = 'Alexander';
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let inventory = ['Fast Sword', ' Light Armor', ' Amulet of Speed', ' and Healing Flask'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let isLeader = false;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log('I am the ' + name +'!');
+console.log('I live in ' + country + ', which is ruled by ' + villian + '.');
+console.log("Yes, the rumors are " + isLeader + ', I am not the leader of ' + country + '.');
+console.log('I am a ' + rank + ', Alongside my friend ' + companion + '.');
+console.log('Yesterday was the festival for the new year, ' + time + ', where I found these. ' + inventory + '.');
